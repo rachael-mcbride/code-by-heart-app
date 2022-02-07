f1d27345ce0752aa6a141700c05f14d7b0c73044
Rachael <mcbride.rac@gmail.com>
2022-02-07 14:27:18 -0500

    Changes landing page

--- src/pages/landing-page/landing-page.component.jsx
+++ src/pages/landing-page/landing-page.component.jsx
@@ -13,8 +13,8 @@ const LandingPage = () => (
         <div className="section1-title">About the App</div>
         <div className="section1-body">
           <span>One of the most effective ways to learn something new is through <b>spaced repetition</b>—a technique that involves reviewing and recalling information at increasing intervals of time until knowledge is fully embedded in long-term memory.</span>
-          <span>Code By Heart is an application specifically <b>designed for developers and programming language learners</b> that leverages this memory technique and is based upon the <a href="https://www.supermemo.com/en/archives1990-2015/english/ol/sm2" target="_blank">SM-2 spaced repetition algorithm</a>.</span>
-          <span>It allows you to <b>create</b>, <b>execute</b>, and <b>save flashcards</b> in a particular programming language, and then later review these cards at optimally-spaced intervals—thus helping you learn code <a href="https://dictionary.cambridge.org/us/dictionary/english/by-heart" target="_blank">by heart</a>.</span>
+          <span>Code By Heart is an application specifically <b>designed for developers and programming language learners</b> that leverages this memory technique and is based upon the <a href="https://www.supermemo.com/en/archives1990-2015/english/ol/sm2" target="_blank" rel="noreferrer">SM-2 spaced repetition algorithm</a>.</span>
+          <span>It allows you to <b>create</b>, <b>execute</b>, and <b>save flashcards</b> in a particular programming language, and then later review these cards at optimally-spaced intervals—thus helping you learn code <a href="https://dictionary.cambridge.org/us/dictionary/english/by-heart" target="_blank" rel="noreferrer">by heart</a>.</span>
         </div>
         <div className="section1-body2">
           <div className="section1-body2-subtitle">
@@ -22,22 +22,22 @@ const LandingPage = () => (
           </div>
           <ul className="section1-body2-list">
             <li>
-              <a href="https://sive.rs/srs" target="_blank">
+              <a href="https://sive.rs/srs" target="_blank" rel="noreferrer">
                 Why one developer described spaced repetition as "<b>the most helpful learning technique I've found in 14 years of computer programming</b>"
               </a>
             </li>
             <li>
-              <a href="https://senrigan.io/blog/chasing-10x-leveraging-a-poor-memory-in-software-engineering/" target="_blank">
+              <a href="https://senrigan.io/blog/chasing-10x-leveraging-a-poor-memory-in-software-engineering/" target="_blank" rel="noreferrer">
                 Why another developer says spaced repetition "<b>saved my software career</b>"
               </a>
             </li>
             <li>
-              <a href="https://www.freecodecamp.org/news/why-i-studied-full-time-for-8-months-for-a-google-interview-cc662ce9bb13#.3d9qfnhq5" target="_blank">
+              <a href="https://www.freecodecamp.org/news/why-i-studied-full-time-for-8-months-for-a-google-interview-cc662ce9bb13#.3d9qfnhq5" target="_blank" rel="noreferrer">
                 How spaced repetition helped one developer <b>prepare for a big interview</b>
               </a>
             </li>
             <li>
-              <a href="https://leetcode.com/discuss/general-discussion/1124635/how-to-leetcode-what-i-learned-from-a-year-of-leetcoding-challenge-problems" target="_blank">
+              <a href="https://leetcode.com/discuss/general-discussion/1124635/how-to-leetcode-what-i-learned-from-a-year-of-leetcoding-challenge-problems" target="_blank" rel="noreferrer">
                 How spaced repetition can help you <b>recognize problem-solving patterns</b> for tough technical interviews more broadly
               </a>
             </li>
